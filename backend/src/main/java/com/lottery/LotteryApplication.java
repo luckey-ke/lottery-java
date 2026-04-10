5a9a6e97e3147b95df7ea05a35017b046c46acbf
yueke <yueke@163.com>
2026-04-10 21:28:18 +0800

    fix

--- backend/src/main/java/com/lottery/LotteryApplication.java
+++ backend/src/main/java/com/lottery/LotteryApplication.java
@@ -1,6 +1,7 @@
 package com.lottery;
 
-import com.baomidou.mybatisplus.annotation.MapperScan;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
